28b24e05cd9030db3e9e94c889afc7c3012f400f
Luuk <luukhafkamp@gmail.com>
2018-06-04 17:51:59 +0200

    Created and styled a search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,16 @@ class Header extends Component {
 	render() {
 		return (
 			<div id="header">
-				<ul>
-					<a href="https://phusionpassenger.com/"><img src="/img/passenger_logo.svg" alt="home" /></a>
-					<li><NavLink to='/tutorials/' activeClassName="selected">Tutorials</NavLink></li>
-					<li><NavLink to='/advanced/' activeClassName="selected">Advanced guides</NavLink></li>
-					<li><NavLink to='/references/' activeClassName="selected">References</NavLink></li>
-					<li><a href="https://www.phusionpassenger.com/library/">Old docs</a></li>
-					{/* <a href="https://www.phusionpassenger.com/install" className="light-button">Free trial</a> */}
-				</ul>
+				<div>
+					<ul>
+						<a href="https://phusionpassenger.com/"><img src="/img/passenger_logo.svg" alt="home" /></a>
+						<li><NavLink to='/tutorials/' activeClassName="selected">Tutorials</NavLink></li>
+						<li><NavLink to='/advanced/' activeClassName="selected">Advanced guides</NavLink></li>
+						<li><NavLink to='/references/' activeClassName="selected">References</NavLink></li>
+						<li><a href="https://www.phusionpassenger.com/library/">Old docs</a></li>
+					</ul>
+					<input type="search" placeholder="search docs.."/>
+				</div>
 			</div>
 		);
 	}
