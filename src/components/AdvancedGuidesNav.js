6cdb2192342d35fd37ef11f77efeb8f94efb8e3f
Luuk <luukhafkamp@gmail.com>
2018-06-15 12:46:43 +0200

    Changed NavLink in Advanced to not be exact

--- src/components/AdvancedGuidesNav.js
+++ src/components/AdvancedGuidesNav.js
@@ -6,13 +6,13 @@ class AdvancedGuidesNav extends Component {
 		return (
 			<ul>
 				<span>Advanced guides</span>
-				<li><NavLink exact to={'/advanced_guides/install_and_upgrade/'} activeClassName="selected">(un)Install & upgrade</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/developing_with_passenger/'} activeClassName="selected">Developing with Passenger</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/config_and_optimization/'} activeClassName="selected">Config and optimization</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/deployment_and_scaling/'} activeClassName="selected">Deployment and scaling</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/docker_support/'} activeClassName="selected">Docker support</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/troubleshooting/'} activeClassName="selected">Troubleshooting</NavLink></li>
-				<li><NavLink exact to={'/advanced_guides/in_depth/'} activeClassName="selected">In-depth</NavLink></li>
+				<li><NavLink to={'/advanced_guides/install_and_upgrade/'} activeClassName="selected">(un)Install & upgrade</NavLink></li>
+				<li><NavLink to={'/advanced_guides/developing_with_passenger/'} activeClassName="selected">Developing with Passenger</NavLink></li>
+				<li><NavLink to={'/advanced_guides/config_and_optimization/'} activeClassName="selected">Config and optimization</NavLink></li>
+				<li><NavLink to={'/advanced_guides/deployment_and_scaling/'} activeClassName="selected">Deployment and scaling</NavLink></li>
+				<li><NavLink to={'/advanced_guides/docker_support/'} activeClassName="selected">Docker support</NavLink></li>
+				<li><NavLink to={'/advanced_guides/troubleshooting/'} activeClassName="selected">Troubleshooting</NavLink></li>
+				<li><NavLink to={'/advanced_guides/in_depth/'} activeClassName="selected">In-depth</NavLink></li>
 			</ul>
 		);
 	}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Dropdown extends Component {
 					<ul>
 						<li>Follow us on: <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwww.phusionpassenger.com%2Flibrary%2F&ref_src=twsrc%5Etfw&screen_name=phusion_nl&tw_p=followbutton"><img src="/img/footer/twitter.svg" alt="twitter"/></a></li>
 						<li>Passenger: <a href="https://github.com/phusion/passenger"><img src="/img/footer/github.svg" alt="Passenger Github"/></a></li>
-						<li>Passenger docs: <a href="#"><img src="/img/footer/github.svg" alt="Passenger docs Github"/></a></li>
+						<li>Passenger docs: <a href=""><img src="/img/footer/github.svg" alt="Passenger docs Github"/></a></li>
 					</ul>
 					<div>
 						<p>Copyright © 2008-2018 Phusion Holding B.V. and contributors to the Passenger Library.</p>
