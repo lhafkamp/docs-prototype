4a44cc035c98134a3004661deb9c6bcd37f0637e
Luuk <luukhafkamp@gmail.com>
2018-06-21 16:51:07 +0200

    Change staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 server "passenger_library@staging.phusionpassenger.com", :roles => [:app, :web, :db]
 # set :application, 'passenger_website'
-# set :branch, 'staging'
+# set :branch, 'device-lab'
 
 
 # server-based syntax
