4ec28fdfb03ae00d7cc6479fea859264576af567
Luuk <luukhafkamp@gmail.com>
2018-06-22 11:43:48 +0200

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Dropdown extends Component {
 					<p>Documentation licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY SA 4.0</a>.</p>
 					<ul>
 						<li>Follow us on: <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwww.phusionpassenger.com%2Flibrary%2F&ref_src=twsrc%5Etfw&screen_name=phusion_nl&tw_p=followbutton"><img src="/img/footer/twitter.svg" alt="twitter"/></a></li>
-						<li>Passenger: <a href="https://github.com/phusion/passenger"><img src="/img/footer/github.svg" alt="Passenger Github"/></a></li>
+						<li>Passenger: <a href="https://github.com/phusion/passenger"><img src={process.env.PUBLIC_URL + '/img/footer/github.svg'} alt="Passenger Github"/></a></li>
 						<li>Passenger docs: <a href=""><img src="/img/footer/github.svg" alt="Passenger docs Github"/></a></li>
 					</ul>
 					<div>
