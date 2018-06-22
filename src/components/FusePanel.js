568e2399dcecdbe1827af1ef1ccc03131fb90a2c
Luuk <luukhafkamp@gmail.com>
2018-06-22 13:16:01 +0200

    Fixed image path

--- src/components/FusePanel.js
+++ src/components/FusePanel.js
@@ -6,7 +6,7 @@ class FusePanel extends Component {
 	render() {
 		return (
 			<div id="fuse-panel">
-				<img src="/img/FP_logo.svg" alt="Fuse Panel logo"/>
+				<img src={`${process.env.PUBLIC_URL}/img/FP_logo.svg`} alt="Fuse Panel logo"/>
 				<img className="fuse" src={`${process.env.PUBLIC_URL}/img/fuse_panel.png`} alt="Fuse Panel"/>
 				<a className="light-button" href="https://www.phusionpassenger.com/fuse-panel">Try the beta</a>
 				<img src={`${process.env.PUBLIC_URL}/img/feedback_placeholder.svg`} alt="feedback"/>
