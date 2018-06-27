9f746571036128a7fa1c33b27905e2cd288f8a63
Luuk <luukhafkamp@gmail.com>
2018-06-27 15:43:44 +0200

    Added more polyfills

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './polyfillTest';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/fn/array/includes';
+import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
-import 'core-js/fn/array/includes';
 
 import App from './App';
 
